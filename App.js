699258689cae3574e25052194e5586ef19253226
goodguytim1 <timobrien56@knights.ucf.edu>
2021-03-10 08:29:51 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, TextInput,  View } from 'react-native';
+import Profile from './Profile'
 
 export default function App() {
+  const [name, setName] = useState('Tim');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <TextInput value={name} onChangeText={(text) => setName(text)}/>
+      <Profile text='Hello World'/>
+      <Profile text={name}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: "white",
   },
 });
---
+++ Profile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Profile({text}) {
+    return (
+        <View>
+            <Text>{text}</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
